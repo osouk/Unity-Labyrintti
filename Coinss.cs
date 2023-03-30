fb9a0b8cb7d359841d78e3639d3cc5bded905ece
osouk <118250606+osouk@users.noreply.github.com>
2023-03-30 13:28:30 +0300

    30.3 Added Sound Effect

--- Coinss.cs
+++ Coinss.cs
@@ -4,31 +4,63 @@ using UnityEngine;
 
 public class Coinss : MonoBehaviour
 {
+    public AudioClip SoundToPlay;
+
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+
     public int Pisteet = 0;
-    public GameObject uiObject; 
+
+    public GameObject uiObject;
+    public GameObject uiObject2;
+    public GameObject uiObject3;
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 100, 20), "Kävyt: " + Pisteet);
+        GUI.Label(new Rect(10, 10, 150, 30), "Pinecones: " + Pisteet);
     }
 
     void Start()
     {
+        audio = GetComponent<AudioSource>();
         uiObject.SetActive(false);
+        uiObject2.SetActive(false);
+        uiObject3.SetActive(false);
     }
 
     private void Update()
     {
-        if(Pisteet == 8)
+        if(Pisteet >= 5)
+        {
+            uiObject3.SetActive(false);
+            uiObject2.SetActive(true);
+            if (!alreadyPlayed)
+            {
+                audio.PlayOneShot(SoundToPlay);
+                alreadyPlayed = true;
+            }
+        }
+        else
+        {           
+            uiObject3.SetActive(true);
+        }
+
+      /*  if(Pisteet == 8)
         {
             uiObject.SetActive(true);
             StartCoroutine("odota");
-        }
+        } */
     }
 
     IEnumerator odota()
     {
         yield return new WaitForSeconds(5);
         Destroy(uiObject);
+    }
+
+    IEnumerator odota2()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject2);
     }
 }
