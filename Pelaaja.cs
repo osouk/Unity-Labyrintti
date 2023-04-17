ed1f0ad4286339d4178bcd0be62813a8aee4db9d
osouk <118250606+osouk@users.noreply.github.com>
2023-04-17 10:35:01 +0300

    Update Pelaaja.cs

--- Pelaaja.cs
+++ Pelaaja.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Pelaaja : MonoBehaviour
 {
+    public Transform orientation;
     public Vector2 turn;
+
+    public GameObject uiobject;
 
     public float sensitivity = .5f;
 
@@ -14,6 +18,7 @@ public class Pelaaja : MonoBehaviour
     public float MaxSpeed = 2f;
 
     public float AmmusNopeus = 200;
+
     Rigidbody rb;
 
     public GameObject Ammus;
@@ -24,32 +29,53 @@ public class Pelaaja : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, -50, 0);
     }
     private void Update()
     {
         turn.x += Input.GetAxis("Mouse X") * sensitivity;
         turn.y += Input.GetAxis("Mouse Y") * sensitivity;
         transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             GameObject go = Instantiate(Ammus, Piipunpaa.position, Quaternion.identity);
             Rigidbody rb = go.GetComponent<Rigidbody>();
             rb.AddForce(Kamera.forward * AmmusNopeus);
         }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+            uiobject.SetActive(true);
+            StartCoroutine(WaitForSec());
+            }
+            
     }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(10);
+        uiobject.SetActive(false);
+
+    }
+
     private void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        float x2 = Input.GetAxis("Horizontal2");
-        float z2 = Input.GetAxis("Vertical2");
-        Vector3 suunta = transform.forward * z + transform.right * x;
-        suunta.y = 0;
-            rb.AddForce(suunta * Speed * Time.fixedDeltaTime);
-        rb.velocity.Normalize();
+        
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            float x2 = Input.GetAxis("Horizontal2");
+            float z2 = Input.GetAxis("Vertical2");
+            Vector3 suunta = transform.forward * z + transform.right * x;
+            rb.velocity = suunta * Speed;
+            suunta.y = 0;
+           // rb.AddForce(suunta * Speed * Time.fixedDeltaTime);
+      //  rb.velocity.Normalize();
+
     }
 }
